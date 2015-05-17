1efb41fa8b5475fe60ef5a60ef04d16315d752c0
binarybruin <michelle.bruin@gmail.com>
2015-05-17 00:00:24 -0400

    first commit

---
+++ Assets/FollowCursor.js
@@ -0,0 +1,21 @@
+﻿var smooth = 5.0;
+
+function Start () {
+
+}
+
+function Update () {
+	var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	var hit : RaycastHit;
+	if(Physics.Raycast(ray,hit,50)){
+		if(hit.transform.gameObject.tag == "cursorfollow"){
+			Drag(hit);
+		}
+	}
+}
+
+function Drag(_h : RaycastHit){ 
+	transform.position = Vector3.Lerp (
+	transform.position, _h.point,
+	Time.deltaTime * smooth);
+}
